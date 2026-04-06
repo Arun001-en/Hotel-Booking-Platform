56d2cbb9c315f3aa02a481a2e8b95a364008d1d9
ayushdixit52 <ayushdixit583@gmail.com>
2026-04-06 19:28:01 +0530

    admin dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,9 @@ import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
+import HotelReg from './components/HotelReg'
+import Layout from './pages/hotelOwner/Layout'
+import Dashboard from './pages/hotelOwner/Dashboard'
 
 const App = () => {
 
@@ -15,6 +18,7 @@ const App = () => {
     <div>
 
       {!isOwnerPath && <Navbar/>}
+      {false && <HotelReg/>}
 
       <div className='min-h-[70vh]'>
         <Routes>
@@ -22,6 +26,12 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms/>}/>
           <Route path='/rooms/:id' element={<RoomDetails/>}/>
           <Route path='/my-bookings' element={<MyBookings/>}/>
+          <Route path='/owner' element={<Layout/>}>
+             <Route index element={<Dashboard/>}/>
+             <Route path='add-room' element={<AddRoom/>}/>
+             <Route path='list-room' element={<ListRoom/>} />
+             
+          </Route>
         </Routes>
       </div>
       <Footer/>
